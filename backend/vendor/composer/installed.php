78c2a4ec7a37952731b53206993a9d920d5bf3b0
unknown <ola.ses@yahoo.com>
2023-07-25 07:28:35 +0100

    Build frontend and backend resources

--- backend/vendor/composer/installed.php
+++ backend/vendor/composer/installed.php
@@ -1,11 +1,11 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => 'a2bb2908c1732c33c699a6fd8eab540b69606349',
         'name' => 'laravel/laravel',
         'dev' => false,
     ),
@@ -314,12 +314,12 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => 'a2bb2908c1732c33c699a6fd8eab540b69606349',
             'dev_requirement' => false,
         ),
         'laravel/sanctum' => array(
